d9b01a44594a11fd9d26a0d665fc41d874a6fd1b
aota18 <sapphire031794@gmail.com>
2023-09-18 14:09:29 -0700

    Type update

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import useSWR from 'swr';
 
-const fetcher = (...args) => fetch (...args).then((res) => res.json());
+const fetcher = (...args:any[]) => fetch (...args).then((res) => res.json());
 
 
 export default function Home() {
